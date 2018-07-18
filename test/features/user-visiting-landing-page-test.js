5e5a8336938d06fd1725fa0a7987ef79b1a1dd14
Tony Susi <tony.susi@wisegroup.co.nz>
2018-07-19 10:55:23 +1200

    Step 3

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,10 @@
 const {assert} = require('chai');
+
+describe('User visits the landing page', () => {
+    describe('On first visit', () => {
+      it('no videos exist', () => {
+        browser.url('/');
+        assert.include(browser.getText('#videos-container'),'');
+      });
+    });
+});
