3de96f3495600f200a8ffca85dd768a0ab5888f8
Tony Susi <tony.susi@wisegroup.co.nz>
2018-07-20 11:49:58 +1200

    Step 12 & 13

--- routes/videos.js
+++ routes/videos.js
@@ -1,12 +1,22 @@
+const Video = require('../models/video');
 const router = require('express').Router();
-
 
 router.get('/videos', async (req, res, next) => {
   res.render('videos');
 });
 
 router.post('/videos', async (req, res, next) => {
-  res.status(201).render('videos');
+
+  const {title, description} = req.body;
+  const newVideo = new Video({title, description});
+  newVideo.validateSync();
+  if (newVideo.errors) {
+    res.status(400).render('videos', {newVideo: newVideo});
+  } else {
+    await newVideo.save();
+    res.status(201).render('videos');
+  }
+
 });
 
 module.exports = router;
--- test/routes/video-test.js
+++ test/routes/video-test.js
@@ -1,9 +1,11 @@
+const Video = require('../../models/video');
 const {assert} = require('chai');
 const request = require('supertest');
 
 const app = require('../../app');
 
 const {connectDatabase, disconnectDatabase} = require('../database-utilities');
+// const {parseTextFromHTML, seedVideoToDatabase} = require('../test-utils');
 
 describe('Server path: /videos', () => {
   beforeEach(connectDatabase);
@@ -16,6 +18,21 @@ describe('Server path: /videos', () => {
         .post('/videos');
       assert.equal(response.status, 201);
       // assert.equal(response.headers.location, '/');
+    });
+    it('renders an video with a title and description', async () => {
+      const videoToCreate = {
+        title: 'Video Title',
+        description: 'Video Description'
+      };
+
+      const response = await request(app)
+      .post('/videos')
+      .type('form')
+      .send(videoToCreate);
+
+      const createdVideo = await Video.findOne(videoToCreate);
+      assert.isOk(createdVideo, 'Video was not created successfully in the database');
+
     });
   });
 });
