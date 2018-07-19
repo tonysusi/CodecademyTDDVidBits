3efe8bf5795eef640511909dfe4189a5eb9908e3
Tony Susi <tony.susi@wisegroup.co.nz>
2018-07-19 16:19:57 +1200

    Step 10

--- test/routes/new-video-test.js
+++ test/routes/new-video-test.js
@@ -3,19 +3,34 @@ const request = require('supertest');
 
 const app = require('../../app');
 
+const {connectDatabase, disconnectDatabase} = require('../database-utilities');
 
 describe('Server path: /videos', () => {
-  // beforeEach(connectDatabaseAndDropData);
-  //
-  // afterEach(diconnectDatabase);
+  beforeEach(async () => {
+    await mongoose.connect(databaseUrl, options);
+    await mongoose.connection.db.dropDatabase();
+  });
+
+  afterEach(async () => {
+     await mongoose.disconnect();
+  });
 
   // Write your test blocks below:
   describe('POST', () => {
-    it('post new video /videos', async () => {
+    it('posts new video', async () => {
       const response = await request(app)
         .post('/videos');
       assert.equal(response.status, 201);
       // assert.equal(response.headers.location, '/');
+    });
+    it('renders an video with a title and image', async () => {
+
+      const response = await request(app)
+      .post(`/video`);
+      //
+      // assert.include(parseTextFromHTML(response.text, '.item-title'), item.title);
+      // const imageElement = findImageElementBySource(response.text, item.imageUrl);
+      // assert.equal(imageElement.src, item.imageUrl);
     });
   });
 });
