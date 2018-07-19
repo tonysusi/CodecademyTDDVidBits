7e94a280072e0d5b93c52e13b0a60167e67f9514
Tony Susi <tony.susi@wisegroup.co.nz>
2018-07-19 13:04:16 +1200

    Step 6

---
+++ test/features/user-visiting-create-page-test.js
@@ -0,0 +1,10 @@
+const {assert} = require('chai');
+
+describe('User visits the create page', () => {
+    describe('Can upload video', () => {
+      it('no videos exist', () => {
+        // browser.url('/');
+        // assert.include(browser.getText('#videos-container'),'');
+      });
+    });
+});
--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,10 +1,20 @@
 const {assert} = require('chai');
 
 describe('User visits the landing page', () => {
-    describe('On first visit', () => {
-      it('no videos exist', () => {
+    describe('views content on first visit', () => {
+      it(' and no videos exist', () => {
         browser.url('/');
         assert.include(browser.getText('#videos-container'),'');
+      });
+    });
+    describe('clicks on button', () => {
+      it('navigates to create page', () => {
+        // Setup
+        browser.url('/');
+        // Exercise
+        browser.click('a.create-video-button');
+        // Verification
+        assert.include(browser.getText('body'), 'Create');
       });
     });
 });
